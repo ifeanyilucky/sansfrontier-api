562574041bc133bcffd5048fceb8b1bd70420230
ifeanyilucky <wfguru2017@gmail.com>
2022-08-12 21:02:32 +0100

    updates

--- utils/multer.js
+++ utils/multer.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const storage = multer.diskStorage({
-  destination: './storage/media',
+  destination: './tmp',
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname);
   },
